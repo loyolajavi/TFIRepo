3c270c41d484c705f6e43b5b8326ae1d8ea8f4d8
marcoshmartinez <mhernanmartinez@gmail.com>
2016-11-05 01:06:34 -0300

    Agrege propert URI de UsuarioEntidad

--- TFISolucion/Entidades/UsuarioEntidad.cs
+++ TFISolucion/Entidades/UsuarioEntidad.cs
@@ -2,110 +2,66 @@ using System;
 
 namespace TFI.Entidades
 {
-	public class UsuarioEntidad
-	{
-		#region Constructors
+    public class UsuarioEntidad
+    {
+        #region Properties
+        /// <summary>
+        /// Gets or sets the IdUsuario value.
+        /// </summary>
+        public int IdUsuario { get; set; }
 
-		/// <summary>
-		/// Initializes a new instance of the UsuarioEntidad class.
-		/// </summary>
-		public UsuarioEntidad()
-		{
-		}
+        /// <summary>
+        /// Gets or sets the IdCondicionFiscal value.
+        /// </summary>
+        public int? IdCondicionFiscal { get; set; }
 
-		/// <summary>
-		/// Initializes a new instance of the UsuarioEntidad class.
-		/// </summary>
-        public UsuarioEntidad(int? idCondicionFiscal, int idUsuarioTipo, string nombre, string apellido, string nroIdentificacion, string CUIT, string email, string nombreUsuario, string clave, DateTime? fecBaja)
-		{
-			this.IdCondicionFiscal = idCondicionFiscal;
-			this.IdUsuarioTipo = idUsuarioTipo;
-			this.Nombre = nombre;
-			this.Apellido = apellido;
-            this.NroIdentificacion = nroIdentificacion;
-			this.CUIT = CUIT;
-			this.Email = email;
-			this.NombreUsuario = nombreUsuario;
-			this.Clave = clave;
-			this.FecBaja = fecBaja;
-		}
+        /// <summary>
+        /// Gets or sets the IdUsuarioTipo value.
+        /// </summary>
+        public int IdUsuarioTipo { get; set; }
 
-		/// <summary>
-		/// Initializes a new instance of the UsuarioEntidad class.
-		/// </summary>
-		public UsuarioEntidad(int idUsuario, int? idCondicionFiscal, int idUsuarioTipo, string nombre, string apellido, string nroIdentificacion, string CUIT, string email, string nombreUsuario, string clave, DateTime? fecBaja)
-		{
-			this.IdUsuario = idUsuario;
-			this.IdCondicionFiscal = idCondicionFiscal;
-			this.IdUsuarioTipo = idUsuarioTipo;
-			this.Nombre = nombre;
-			this.Apellido = apellido;
-			this.NroIdentificacion = nroIdentificacion;
-			this.CUIT = CUIT;
-			this.Email = email;
-			this.NombreUsuario = nombreUsuario;
-			this.Clave = clave;
-			this.FecBaja = fecBaja;
-		}
+        /// <summary>
+        /// Gets or sets the Nombre value.
+        /// </summary>
+        public string Nombre { get; set; }
 
-		#endregion
+        /// <summary>
+        /// Gets or sets the Apellido value.
+        /// </summary>
+        public string Apellido { get; set; }
 
-		#region Properties
-		/// <summary>
-		/// Gets or sets the IdUsuario value.
-		/// </summary>
-		public int IdUsuario { get; set; }
-
-		/// <summary>
-		/// Gets or sets the IdCondicionFiscal value.
-		/// </summary>
-		public int? IdCondicionFiscal { get; set; }
-
-		/// <summary>
-		/// Gets or sets the IdUsuarioTipo value.
-		/// </summary>
-		public int IdUsuarioTipo { get; set; }
-
-		/// <summary>
-		/// Gets or sets the Nombre value.
-		/// </summary>
-		public string Nombre { get; set; }
-
-		/// <summary>
-		/// Gets or sets the Apellido value.
-		/// </summary>
-		public string Apellido { get; set; }
-
-		/// <summary>
-		/// Gets or sets the Dni value.
-		/// </summary>
+        /// <summary>
+        /// Gets or sets the Dni value.
+        /// </summary>
         public string NroIdentificacion { get; set; }
 
-		/// <summary>
-		/// Gets or sets the CUIT value.
-		/// </summary>
-		public string CUIT { get; set; }
+        /// <summary>
+        /// Gets or sets the CUIT value.
+        /// </summary>
+        public string CUIT { get; set; }
 
-		/// <summary>
-		/// Gets or sets the Email value.
-		/// </summary>
-		public string Email { get; set; }
+        /// <summary>
+        /// Gets or sets the Email value.
+        /// </summary>
+        public string Email { get; set; }
 
-		/// <summary>
-		/// Gets or sets the NombreUsuario value.
-		/// </summary>
-		public string NombreUsuario { get; set; }
+        /// <summary>
+        /// Gets or sets the NombreUsuario value.
+        /// </summary>
+        public string NombreUsuario { get; set; }
 
-		/// <summary>
-		/// Gets or sets the Clave value.
-		/// </summary>
-		public string Clave { get; set; }
+        /// <summary>
+        /// Gets or sets the Clave value.
+        /// </summary>
+        public string Clave { get; set; }
 
-	    /// <summary>
-		/// Gets or sets the FecBaja value.
-		/// </summary>
-		public DateTime? FecBaja { get; set; }
+        /// <summary>
+        /// Gets or sets the FecBaja value.
+        /// </summary>
+        public DateTime? FecBaja { get; set; }
 
-		#endregion
-	}
+        public string URI { get; set; }
+
+        #endregion
+    }
 }
